2b55afc0a7d88bda810ef249997db63d9aa51ea6
v0idpwn <v0idpwn@gmail.com>
2021-08-09 19:16:41 +0300

    chore: bootstrap elixir project

---
+++ lib/blockfrost.ex
@@ -0,0 +1,18 @@
+defmodule Blockfrost do
+  @moduledoc """
+  Documentation for `Blockfrost`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blockfrost.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule Blockfrost.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :blockfrost,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto, "~> 3.6"},
+      {:finch, "~> 0.8.0"}
+    ]
+  end
+end
