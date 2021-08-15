a0fc1d0221c66a111f836fc1265d9c1ae1b97eea
v0idpwn <v0idpwn@gmail.com>
2021-08-15 13:58:22 +0300

    ci: setup github actions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Blockfrost.MixProject do
     [
       app: :blockfrost,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
