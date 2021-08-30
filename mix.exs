c9cb8d922ab8a41d451927cac87041fbeebe8676
v0idpwn <v0idpwn@gmail.com>
2021-08-30 08:01:04 +0300

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Blockfrost.MixProject do
   def project do
     [
       app: :blockfrost,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
