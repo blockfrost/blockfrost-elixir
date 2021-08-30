9de3d8463ef069bffd6f84d2c3a13e5277e0d190
v0idpwn <v0idpwn@gmail.com>
2021-08-30 08:00:54 +0300

    docs: fix missing documentation in Cardano.Metadata

--- lib/blockfrost/cardano/metadata.ex
+++ lib/blockfrost/cardano/metadata.ex
@@ -10,6 +10,11 @@ defmodule Blockfrost.Cardano.Metadata do
   }
 
   @doc """
+  List of all used transaction metadata labels.
+
+  Supports pagination.
+
+  [API Docs](https://docs.blockfrost.io/#tag/Cardano-Metadata/paths/~1metadata~1txs~1labels/get)
   """
   @spec transaction_metadata_labels(Blockfrost.t(), Keyword.t()) ::
           {:ok, TransactionMetadataLabelsResponse.t()} | HTTP.error_response()
@@ -25,6 +30,13 @@ defmodule Blockfrost.Cardano.Metadata do
     |> Response.deserialize(TransactionMetadataLabelsResponse)
   end
 
+  @doc """
+  Transaction metadata per label.
+
+  The json data is automatically parsed.
+
+  [API Docs](https://docs.blockfrost.io/#tag/Cardano-Metadata/paths/~1metadata~1txs~1labels~1{label}/get)
+  """
   @spec transaction_metadata_content_json(Blockfrost.t(), String.t(), Keyword.t()) ::
           {:ok, TransactionMetadataContentJSONResponse.t()} | HTTP.error_response()
   def transaction_metadata_content_json(name, label, opts \\ []) do
@@ -39,6 +51,11 @@ defmodule Blockfrost.Cardano.Metadata do
     |> Response.deserialize(TransactionMetadataContentJSONResponse)
   end
 
+  @doc """
+  Transaction metadata per label.
+
+  [API Docs](https://docs.blockfrost.io/#tag/Cardano-Metadata/paths/~1metadata~1txs~1labels~1{label}~1cbor/get)
+  """
   @spec transaction_metadata_content_cbor(Blockfrost.t(), String.t(), Keyword.t()) ::
           {:ok, TransactionMetadataContentCBORResponse.t()} | HTTP.error_response()
   def transaction_metadata_content_cbor(name, label, opts \\ []) do
